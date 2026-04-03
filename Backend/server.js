953d07c99df9b477c83d94526946b97b58ec8ec6
satyam kumar mishra <satyamkmishraa@gmail.com>
2026-04-03 23:18:30 +0530

    Fix: Backend CORS array and environment variables

--- Backend/server.js
+++ Backend/server.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(cors ({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "https://event-orcin-one.vercel.app"],
     credentials: true
 }));
 
