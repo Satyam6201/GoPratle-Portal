67c1c284fe14c5768e7311e57989251c698e4090
satyam kumar mishra <satyamkmishraa@gmail.com>
2026-04-04 00:01:31 +0530

    final commit

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: (process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8080').replace(/\/$/, '') + '/api',
+  baseURL: 'https://gopratle-portal.onrender.com/api', 
   headers: {
     'Content-Type': 'application/json',
   },
